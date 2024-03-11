2334a1b63f2942da25da6c6baa565e50a36d2509
roydenwa <royden-wagner@outlook.com>
2024-03-11 16:56:11 +0100

    Format code

--- sfa/ros_utils.py
+++ sfa/ros_utils.py
@@ -24,7 +24,12 @@ import config.kitti_config as cnf
 from data_process.transformation import lidar_to_camera_box
 from utils.visualization_utils import merge_rgb_to_bev, show_rgb_image_with_boxes
 from data_process.kitti_data_utils import Calibration
-from utils.demo_utils import parse_demo_configs, do_detect, download_and_unzip, write_credit
+from utils.demo_utils import (
+    parse_demo_configs,
+    do_detect,
+    download_and_unzip,
+    write_credit,
+)
 
 from data_process.kitti_data_utils import get_filtered_lidar
 from data_process.kitti_bev_utils import makeBEVMap
@@ -48,7 +53,15 @@ def read_lidar_bin(path):
     return np.fromfile(path, dtype=np.float32).reshape(-1, 4)
 
 
-def load_bevmap(pcd, boundary: dict = None, n_lasers: int = 128, center_y: bool = True, is_back: bool = False, is_left: bool = False, is_right: bool = False):
+def load_bevmap(
+    pcd,
+    boundary: dict = None,
+    n_lasers: int = 128,
+    center_y: bool = True,
+    is_back: bool = False,
+    is_left: bool = False,
+    is_right: bool = False,
+):
     if not boundary:
         lidar = get_filtered_lidar(pcd, cnf.boundary)
         bevmap = makeBEVMap(lidar, cnf.boundary, n_lasers, center_y)
@@ -69,25 +82,49 @@ def load_bevmap(pcd, boundary: dict = None, n_lasers: int = 128, center_y: bool 
     return bevmap
 
 
-def detect(configs, model, bevmaps, peak_thresh: float = None, considered_classes: tuple = ([0, 1, 2], [0], [0, 1, 2])):
+def detect(
+    configs,
+    model,
+    bevmaps,
+    peak_thresh: float = None,
+    considered_classes: tuple = ([0, 1, 2], [0], [0, 1, 2]),
+):
     input_bev_maps = bevmaps.to(configs.device, non_blocking=True).float()
-    
+
     t1 = time_synchronized()
-    
+
     outputs = model(input_bev_maps)
-    outputs['hm_cen'] = _sigmoid(outputs['hm_cen'])
-    outputs['cen_offset'] = _sigmoid(outputs['cen_offset'])
-    
+    outputs["hm_cen"] = _sigmoid(outputs["hm_cen"])
+    outputs["cen_offset"] = _sigmoid(outputs["cen_offset"])
+
     # detections size (batch_size, K, 10)
-    detections = decode(outputs['hm_cen'], outputs['cen_offset'], outputs['direction'], outputs['z_coor'],
-                        outputs['dim'], K=configs.K)
+    detections = decode(
+        outputs["hm_cen"],
+        outputs["cen_offset"],
+        outputs["direction"],
+        outputs["z_coor"],
+        outputs["dim"],
+        K=configs.K,
+    )
     detections = detections.cpu().numpy().astype(np.float32)
-    
+
     if peak_thresh:
-        detections = post_processing_batched(detections, configs.num_classes, configs.down_ratio, peak_thresh, considered_classes)
+        detections = post_processing_batched(
+            detections,
+            configs.num_classes,
+            configs.down_ratio,
+            peak_thresh,
+            considered_classes,
+        )
     else:
-        detections = post_processing_batched(detections, configs.num_classes, configs.down_ratio, configs.peak_thresh, considered_classes)
-    
+        detections = post_processing_batched(
+            detections,
+            configs.num_classes,
+            configs.down_ratio,
+            configs.peak_thresh,
+            considered_classes,
+        )
+
     t2 = time_synchronized()
 
     # Inference speed
@@ -108,7 +145,13 @@ def get_yaw(direction):
     return np.arctan2(direction[:, 0:1], direction[:, 1:2])
 
 
-def post_processing_batched(detections, num_classes=3, down_ratio=4, peak_thresh=0.2, considered_classes: tuple = ((0, 1, 2), (0, 1, 2), (0, 1, 2))):
+def post_processing_batched(
+    detections,
+    num_classes=3,
+    down_ratio=4,
+    peak_thresh=0.2,
+    considered_classes: tuple = ((0, 1, 2), (0, 1, 2), (0, 1, 2)),
+):
     """
     :param detections: [batch_size, K, 10] -> K = topk results (default 50)
     # (scores x 1, xs x 1, ys x 1, z_coor x 1, dim x 3, direction x 2, clses x 1)
@@ -121,30 +164,35 @@ def post_processing_batched(detections, num_classes=3, down_ratio=4, peak_thresh
         top_preds = {}
         classes = detections[i, :, -1]
         for j in range(num_classes):
-            inds = (classes == j)
+            inds = classes == j
             # x, y, z, h, w, l, yaw
-            top_preds[j] = np.concatenate([
-                detections[i, inds, 0:1],
-                detections[i, inds, 1:2] * down_ratio,
-                detections[i, inds, 2:3] * down_ratio,
-                detections[i, inds, 3:4],
-                detections[i, inds, 4:5],
-                detections[i, inds, 5:6] / cnf.bound_size_y * cnf.BEV_WIDTH,
-                detections[i, inds, 6:7] / cnf.bound_size_x * cnf.BEV_HEIGHT,
-                get_yaw(detections[i, inds, 7:9]).astype(np.float32)], axis=1)
+            top_preds[j] = np.concatenate(
+                [
+                    detections[i, inds, 0:1],
+                    detections[i, inds, 1:2] * down_ratio,
+                    detections[i, inds, 2:3] * down_ratio,
+                    detections[i, inds, 3:4],
+                    detections[i, inds, 4:5],
+                    detections[i, inds, 5:6] / cnf.bound_size_y * cnf.BEV_WIDTH,
+                    detections[i, inds, 6:7] / cnf.bound_size_x * cnf.BEV_HEIGHT,
+                    get_yaw(detections[i, inds, 7:9]).astype(np.float32),
+                ],
+                axis=1,
+            )
 
             # Filter by peak_thresh
             if len(top_preds[j]) > 0:
-                keep_inds = (top_preds[j][:, 0] > peak_thresh)
+                keep_inds = top_preds[j][:, 0] > peak_thresh
                 top_preds[j] = top_preds[j][keep_inds]
 
             # Filter considered classes
             if j not in considered_classes[i]:
                 top_preds[j] = []
-        
+
         ret.append(top_preds)
 
     return ret
+
 
 def preprocess_point_cloud(pc: pcl.PointCloud) -> np.ndarray:
     pcd_np = pc.to_ndarray()
@@ -214,30 +262,45 @@ def bboxes_to_rosmsg(bboxes: list, timestamp) -> BoundingBoxArray:
 
     return rosboxes
 
+
 @njit
-def bev_center_nms(bboxes_in: np.ndarray, thresh_x: float = 1.0, thresh_y: float = 1.0, class_id: float = 1.0) -> list:
-    bboxes_out = [] # [confidence, cls_id, x, y, z, h, w, l, yaw]
+def bev_center_nms(
+    bboxes_in: np.ndarray,
+    thresh_x: float = 1.0,
+    thresh_y: float = 1.0,
+    class_id: float = 1.0,
+) -> list:
+    bboxes_out = []  # [confidence, cls_id, x, y, z, h, w, l, yaw]
 
     for idx_a, box_a in enumerate(bboxes_in):
         keep = True
         for idx_b, box_b in enumerate(bboxes_in):
-            if idx_a != idx_b and box_a[1] == class_id and box_b[1] == class_id and box_a[0] < box_b[0] and np.abs(box_a[2] - box_b[2]) < thresh_x and np.abs(box_a[3] - box_b[3]) < thresh_y:
+            if (
+                idx_a != idx_b
+                and box_a[1] == class_id
+                and box_b[1] == class_id
+                and box_a[0] < box_b[0]
+                and np.abs(box_a[2] - box_b[2]) < thresh_x
+                and np.abs(box_a[3] - box_b[3]) < thresh_y
+            ):
                 keep = False
         if keep:
             bboxes_out.append(box_a)
-    
+
     return bboxes_out
 
 
 def shutdown_callback(event):
     # opt use rosnode.get_node_names() if rviz not in list kill
     if not "rviz" in "".join(rosnode.get_node_names()):
-    # if not rosgraph.is_master_online():
+        # if not rosgraph.is_master_online():
         os.kill(os.getpid(), signal.SIGTERM)
 
 
 # @njit
-def ego_nms(bboxes_in: np.ndarray, x_thresh: float = 1.5, y_thresh: float = 1.5) -> list:
+def ego_nms(
+    bboxes_in: np.ndarray, x_thresh: float = 1.5, y_thresh: float = 1.5
+) -> list:
     bboxes_out = []
 
     for bbox in bboxes_in:
